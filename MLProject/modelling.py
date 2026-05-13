a5d2c91f10e76540144d8088bb2583f1e871f183
agniftya <agnifatyakholila@gmail.com>
2026-05-13 23:12:26 +0700

    Fix alamat dataset di modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,8 +2,8 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-train_data = pd.read_csv('restaurant_satisfaction_preprocessing/train_preprocessed.csv')
-test_data = pd.read_csv('restaurant_satisfaction_preprocessing/test_preprocessed.csv')
+train_data = pd.read_csv('MLProject/train_preprocessed.csv')
+test_data = pd.read_csv('MLProject/test_preprocessed.csv')
 
 X_train = train_data.drop('HighSatisfaction', axis=1)
 y_train = train_data['HighSatisfaction']
