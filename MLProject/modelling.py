1dd6b8f5a8b52fe75a4c446878a2a18d3bed9afd
agniftya <agnifatyakholila@gmail.com>
2026-05-15 20:20:41 +0700

    Final Fix: Integrate Real ML Model in Inference and Prometheus

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,17 +1,34 @@
 import pandas as pd
+import mlflow
+import joblib
+import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-train_data = pd.read_csv('MLProject/train_preprocessed.csv')
-test_data = pd.read_csv('MLProject/test_preprocessed.csv')
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.sklearn.autolog()
+mlflow.set_experiment("Restaurant_Satisfaction_Experiment")
 
-X_train = train_data.drop('HighSatisfaction', axis=1)
-y_train = train_data['HighSatisfaction']
-X_test = test_data.drop('HighSatisfaction', axis=1)
-y_test = test_data['HighSatisfaction']
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
-model = RandomForestClassifier(random_state=42)
-model.fit(X_train, y_train)
+with mlflow.start_run():
+    train_path = os.path.join(base_dir, 'train_preprocessed.csv')
+    test_path = os.path.join(base_dir, 'test_preprocessed.csv')
+    
+    train_data = pd.read_csv(train_path)
+    test_data = pd.read_csv(test_path)
 
-y_pred = model.predict(X_test)
-print(f"Akurasi Model Dasar: {accuracy_score(y_test, y_pred):.4f}")
+    X_train = train_data.drop('HighSatisfaction', axis=1)
+    y_train = train_data['HighSatisfaction']
+    X_test = test_data.drop('HighSatisfaction', axis=1)
+    y_test = test_data['HighSatisfaction']
+
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    print(f"Akurasi Model Dasar: {accuracy_score(y_test, y_pred):.4f}")
+
+    mlflow.sklearn.log_model(model, "model")
+
+joblib.dump(model, "model.pkl")
